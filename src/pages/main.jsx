3f94873ffba72e7f2f9c1c6a139bab32fb08d112
jjkim112 <jjkim112@naver.com>
2023-04-06 11:50:55 +0900

    shuffle

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Main = () => {
   return (
     <div className="bg-pink-300 min-h-screen flex flex-col justify-center items-center">
-      main
+      <div className="text-6xl font-bold">최고의 월드컵</div>
+      <Link to="/worldcup">
+        <button className="bg-blue-300 text-white px-4 py-2 rounded-lg mt-8">
+          시 작
+        </button>
+      </Link>
     </div>
   );
 };
--- src/pages/worldcup.jsx
+++ src/pages/worldcup.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import animalData from "../animalData.json";
+import { useState } from "react";
+
 const Worldcup = () => {
+  const [shuffleAnimal, setShuffleAnimal] = useState();
+
+  useEffect(() => {
+    let shuffleAnimalData = animalData.sort(() => {
+      return -1;
+    });
+    console.log(shuffleAnimalData);
+  }, []);
   return <div>Worldcup</div>;
 };
 
